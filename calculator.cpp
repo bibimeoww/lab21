11fbaf0623515ebf9998ef67a486ca688a8b0af0
bibimeoww <papitchaya2404@gmail.com>
2025-02-20 20:35:44 +0700

    jaeten

--- calculator.cpp
+++ calculator.cpp
@@ -8,7 +8,7 @@ char textnum1[100];
 char textnum2[100];
 
 HWND TEXT, Plus, Minus, Multiply, Divide, num_1, num_2;
-/* This is where all the input to the window goes to */
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) {
 	switch(Message) {
 		
@@ -47,37 +47,33 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam) 
 					break;
 			}
 			break;
-		/* Upon destruction, tell the main thread to stop */
+
 		case WM_DESTROY: {
 			PostQuitMessage(0);
 			break;
 		}
 		
-		/* All other messages (a lot of them) are processed using default procedures */
 		default:
 			return DefWindowProc(hwnd, Message, wParam, lParam);
 	}
 	return 0;
 }
 
-/* The 'main' function of Win32 GUI programs: this is where execution starts */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	WNDCLASSEX wc; /* A properties struct of our window */
-	HWND hwnd; /* A 'HANDLE', hence the H, or a pointer to our window */
-	MSG msg; /* A temporary location for all messages */
+	WNDCLASSEX wc; 
+	HWND hwnd; 
+	MSG msg; 
 
-	/* zero out the struct and set the stuff we want to modify */
 	memset(&wc,0,sizeof(wc));
 	wc.cbSize	 = sizeof(WNDCLASSEX);
-	wc.lpfnWndProc	 = WndProc; /* This is where we will send messages to */
+	wc.lpfnWndProc	 = WndProc; 
 	wc.hInstance	 = hInstance;
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 	
-	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
 	wc.hbrBackground = CreateSolidBrush(RGB(142,100,227));;
 	wc.lpszClassName = "WindowClass";
-	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
-	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
+	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); 
+	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); 
 
 	if(!RegisterClassEx(&wc)) {
 		MessageBox(NULL, "Window Registration Failed!","Error!",MB_ICONEXCLAMATION|MB_OK);
@@ -96,14 +92,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return 0;
 	}
 
-	/*
-		This is the heart of our program where all input is processed and 
-		sent to WndProc. Note that GetMessage blocks code flow until it receives something, so
-		this loop will not produce unreasonably high CPU usage
-	*/
-	while(GetMessage(&msg, NULL, 0, 0) > 0) { /* If no error is received... */
-		TranslateMessage(&msg); /* Translate key codes to chars if present */
-		DispatchMessage(&msg); /* Send it to WndProc */
+	while(GetMessage(&msg, NULL, 0, 0) > 0) { 
+		TranslateMessage(&msg);
+		DispatchMessage(&msg); 
 	}
 	return msg.wParam;
 }
